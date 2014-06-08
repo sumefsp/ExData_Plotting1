94a5376f03ecde79df6f18b28cb9053a0cad93e2
sumefsp <garnaiksumeet@gmail.com>
2014-06-08 10:49:00 +0530

    Fixed Typo in plot3.R

--- plot3.R
+++ plot3.R
@@ -9,9 +9,9 @@ fileurl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_powe
 download.file(fileurl, destfile="./exdata_data_household_power_consumption.zip", method="curl")
 
 ## lists the contents of the zip file and reads the required data
-fileContenets <- unzip(zipfile="./exdata_data_household_power_consumption.zip", list=T)
+fileContents <- unzip(zipfile="./exdata_data_household_power_consumption.zip", list=T)
 unzip("./exdata_data_household_power_consumption.zip")
-requiredData <- read.csv.sql(fileContenets$Name, sep=";", header=T, sql='select * from file where Date = "1/2/2007" or Date = "2/2/2007"')
+requiredData <- read.csv.sql(fileContents$Name, sep=";", header=T, sql='select * from file where Date = "1/2/2007" or Date = "2/2/2007"')
 
 ## converts the Date and Time to appropriate data types
 requiredData[, 1] <- as.character(requiredData[, 1])
